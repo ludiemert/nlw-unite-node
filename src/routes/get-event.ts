7e5a48fff8d7c9e7bd8dd259cf35b6f14e46e9ce
ludiemert <lucianadiemert@gmail.com>
2024-04-14 16:36:10 -0300

    feat: ajustes na rota get-events

--- src/routes/get-event.ts
+++ src/routes/get-event.ts
@@ -1,8 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
-import {z} from "zod";
+import { z } from "zod";
 import { prisma } from "../lib/prisma";
-
 
 export async function getEvent(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get(
@@ -12,21 +11,52 @@ export async function getEvent(app: FastifyInstance) {
         params: z.object({
           eventId: z.string().uuid(),
         }),
-        response: {},
+        response: { //typed route
+          200: {
+            event: z.object({
+              id: z.string().uuid(),
+              title: z.string(),
+          slug: z.string(),
+          details: z.string().nullable(),
+          maximumAttendees: z.number().int().nullable(),
+          attendeesAmount: z.number().int(),
+            })
+          }
+        },
       },
     },
     async (request, reply) => {
-      const { eventId } = request.params
+      const { eventId } = request.params;
 
       const event = await prisma.event.findUnique({
+        select: {
+          id: true,
+          title: true,
+          slug: true,
+          details: true,
+          maximumAttendees: true,
+          _count: {
+            select: {
+              attendees: true,
+            },
+          },
+        },
         where: {
           id: eventId,
-        }
-      })
+        },
+      });
       if (event === null) {
-        throw new Error('Event not found.')
+        throw new Error("Event not found.");
       }
-      return reply.send({event})
-    }
-  );
+      return reply.send({
+        event: {
+          id: event.id,
+          title: event.title,
+          slug: event.slug,
+          details: event.details,
+          maximumAttendees: event.maximumAttendees,
+          attendeesAmount: event._count.attendees,
+        },
+      });
+    });
 }
